ee034643c4f517e72586cadd0695db32ff4a9b0f
railenine <railenine@gmail.com>
2022-05-29 22:56:29 +0300

    article ready

---
+++ src/components/tagList/tagList.js
@@ -0,0 +1,13 @@
+const TagList = ({tags}) => {
+    return (
+        <ul className="tag-list">
+            {tags.map(tag => (
+              <li key={tag} className="tag-default tag pill tag-outline">
+                {tag}
+              </li>
+            ))}
+        </ul>
+    )
+}
+
+export default TagList;
